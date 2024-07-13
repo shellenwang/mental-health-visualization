c56910fb44d23f7527a4d5871ef14431008a2d3f
Shellen Wang <shellenwangece@gmail.com>
2024-07-12 23:57:29 -0400

    Adding one sample scene

--- docs/script.js
+++ docs/script.js
@@ -1,4 +1,18 @@
-// docs/script.js
 document.addEventListener("DOMContentLoaded", function() {
-    // D3.js code will go here
+    var data = [10, 20, 30, 40, 50];
+
+    var svg = d3.select("#visualization")
+                .append("svg")
+                .attr("width", 500)
+                .attr("height", 500);
+
+    svg.selectAll("rect")
+       .data(data)
+       .enter()
+       .append("rect")
+       .attr("x", (d, i) => i * 60)
+       .attr("y", d => 500 - d * 10)
+       .attr("width", 50)
+       .attr("height", d => d * 10)
+       .attr("fill", "teal");
 });
